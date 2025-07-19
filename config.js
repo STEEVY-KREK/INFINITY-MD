bdb1fa2e5af033e34e84980007de6ac1307df4cf
STEEVY-KREK <steevytech04@gmail.com>
2025-07-19 00:59:29 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put your session id here",
+SESSION_ID: process.env.SESSION_ID || "INFINITY-MD;;;=>6MdmAAqC#ugk6l3c_XxdrzPQw1PmgO4iH608wi0_gbSK2nGOguJY",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -31,9 +31,9 @@ MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/rxuedn.j
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "INFINITY-MD",
+BOT_NAME: process.env.BOT_NAME || "𝐒𝐄𝐍𝐊𝐔-MD",
 // add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "INFINITY-MD",
+STICKER_NAME: process.env.STICKER_NAME || "</sᴛᴇᴇᴠʏ-ᴛᴇᴄʜ♾️/>",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
@@ -45,7 +45,7 @@ OWNER_NUMBER: process.env.OWNER_NUMBER || "50939103464",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "SIRIUS-TECH",
 // add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*> 𝙥𝙤𝙬𝙚𝙧𝙚𝙙 𝙗𝙮 𝙎𝙄𝙍𝙄𝙐𝙎*",
+DESCRIPTION: process.env.DESCRIPTION || "*> 𝙥𝙤𝙬𝙚𝙧𝙚𝙙 𝙗𝙮 </sᴛᴇᴇᴠʏ-ᴛᴇᴄʜ♾️/>*",
 // add bot owner name    
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/rxuedn.jpg",
 // add img for alive msg
@@ -73,7 +73,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "50939103464",
+DEV: process.env.DEV || "50939130904",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
